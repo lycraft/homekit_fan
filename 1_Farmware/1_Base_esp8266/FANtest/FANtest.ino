8ab32d4d48667d7e720b685ccc270468d59b46d0
lycraft <lycraft@qq.com>
2022-05-29 02:41:35 +0800

    esp8266 is ok!!

--- 1_Farmware/1_Base_esp8266/FANtest/FANtest.ino
+++ 1_Farmware/1_Base_esp8266/FANtest/FANtest.ino
@@ -19,12 +19,7 @@
       to remove the previous HomeKit pairing storage and
       enable the pairing with the new accessory of this new HomeKit example.
 
-   For Connection.
-   1. CONNECT SOLID STATE RELAY TO PIN 5
-   2. CONNECT SOLID STATE RELAY TO PIN 4
       
-
-   This example code is only for personal/hobby use not for sale.   
 */
 
 #include <Arduino.h>
@@ -34,25 +29,16 @@
 
 #define LOG_D(fmt, ...)   printf_P(PSTR(fmt "\n") , ##__VA_ARGS__);
 
-bool fan_con = false;   // bool to store fan_condition if_True( Fan is ON) if_False( Fan is OFF)
-float set_pwm = 0;      // Store the pwm vale to give to fan from 0 to 1024 
-float per_send = 0;     // Store the percentage vale to give back to ios app.
-
-#define RELAY 5      // CONNECT SOLID STATE RELAY TO PIN 5
-#define buttonpin 4  // CONNECT SOLID STATE RELAY TO PIN 4
-
 void setup() {
-  Serial.begin(115200);
-  wifi_connect(); // in wifi_info.h
-  pinMode(RELAY, OUTPUT);
-  pinMode(buttonpin, INPUT_PULLUP);
-  homekit_storage_reset(); // to remove the previous HomeKit pairing storage when you first run this new HomeKit example
-  my_homekit_setup();
+    Serial.begin(115200);
+    wifi_connect(); // in wifi_info.h
+    homekit_storage_reset(); // to remove the previous HomeKit pairing storage when you first run this new HomeKit example
+    my_homekit_setup();
 }
 
 void loop() {
-  my_homekit_loop();
-  delay(10);
+    my_homekit_loop();
+    delay(10);
 }
 
 //==============================
@@ -61,92 +47,136 @@ void loop() {
 
 // access your HomeKit characteristics defined in my_accessory.c
 extern "C" homekit_server_config_t config;
-extern "C" homekit_characteristic_t fan_on;
-extern "C" homekit_characteristic_t rotation_speed;
+extern "C" homekit_characteristic_t fan_on;                         //风扇开关
+extern "C" homekit_characteristic_t rotation_speed;                 //速度调节
+extern "C" homekit_characteristic_t cha_switch_on0;  
 
 static uint32_t next_heap_millis = 0;
 
-//Called when the fan value is changed by iOS Home APP
+bool fan_con = false;   // bool to store fan_condition if_True( Fan is ON) if_False( Fan is OFF)
+float set_pwm = 0;      // Store the pwm vale to give to fan from 0 to 1024 
+float per_send = 0;     // Store the percentage vale to give back to ios app.       存储百分比值以回馈给 ios 应用程序。
+
+#define relay0 5        // 总开关继电器
+#define relay1 4        // 低速继电器
+#define relay2 14       // 中速继电器
+#define relay3 12       // 高速继电器
+
+#define button0 0      // 总开关按键
+#define button1 2       // 低速按键
+#define button2 13       // 中速按键
+#define button3 15      // 高速按键
+
+#define PIN_SWITCH0 16
+
+//Called when the fan value is changed by iOS Home APP                  iOS Home APP更改风扇值(开关)时调用
 void fan_on_setter(const homekit_value_t value)
-{ bool fan = value.bool_value;
-  if (fan == 1)
-  { bool fan_con = true;
-    fan_on.value.bool_value = fan_con;
-    per_send = map(set_pwm, 0, 1024, 0, 100);                               //converting values from pwm(0-1024) to %(0-100)
-    rotation_speed.value.float_value = per_send;                             
-    homekit_characteristic_notify(&fan_on, fan_on.value);                   // sending value to ios app
-    homekit_characteristic_notify(&rotation_speed, rotation_speed.value);   // sending value to ios app
-    analogWrite(RELAY, set_pwm);
-    Serial.println("FAN IS ON NOW !!!!!");
-  }
-  else
-  {
-    bool fan_con = false;
-    fan_on.value.bool_value = fan_con;                                       
-    rotation_speed.value.float_value = 0;                                    
-    homekit_characteristic_notify(&fan_on, fan_on.value);                    // sending value to ios app
-    homekit_characteristic_notify(&rotation_speed, rotation_speed.value);    // sending value to ios app
-    analogWrite(RELAY, 0);
-    Serial.println("FAN IS OFF NOW !!!!!");
-  }
+{ 
+    bool fan = value.bool_value;
+    if (fan == 1)
+    {
+        bool fan_con = true;
+        fan_on.value.bool_value = fan_con;
+        per_send = map(set_pwm, 0, 3, 0, 100);                               // 将值从 pwm(0-1024) 转换为 %(0-100)
+        rotation_speed.value.float_value = per_send;                             
+        homekit_characteristic_notify(&fan_on, fan_on.value);                   // sending value to ios app
+        homekit_characteristic_notify(&rotation_speed, rotation_speed.value);   // sending value to ios app
+        // analogWrite(relay0, set_pwm);
+        digitalWrite(relay0, HIGH);                                             //闭合总开关继电器
+        Serial.println("FAN IS ON NOW !!!!!");
+    }
+    else
+    {
+        bool fan_con = false;
+        fan_on.value.bool_value = fan_con;                                       
+        rotation_speed.value.float_value = 0;                                    
+        homekit_characteristic_notify(&fan_on, fan_on.value);                    // sending value to ios app
+        homekit_characteristic_notify(&rotation_speed, rotation_speed.value);    // sending value to ios app
+        digitalWrite(relay0, LOW);
+        Serial.println("FAN IS OFF NOW !!!!!");
+    }
 }
 
-//Called when the rotatio//speed value is changed by iOS Home APP
+//Called when the switch value is changed by iOS Home APP               当收到传来的信息执行的函数
+void cha_switch_on_setter0(const homekit_value_t value) {
+	bool on = value.bool_value;
+	cha_switch_on0.value.bool_value = on;	//sync the value
+	LOG_D("Switch0: %s", on ? "ON" : "OFF");
+	digitalWrite(PIN_SWITCH0, on ? LOW : HIGH);
+}
 
-void rotation_speed_setter(const homekit_value_t v)
-{ float per = v.float_value;
-  per = map(per, 0, 100, 0, 1024);                                       // converting values from %(0-100) to pwm(0-1024) 
-  analogWrite(RELAY, per);                                               // Setting the fan speed 
-  Serial.println(per);
-  set_pwm = per;                                                         // store pwm values to other variable
-  per_send = map(set_pwm, 0, 1024, 0, 100);                              // converting values from pwm(0-1024) to %(0-100)
-  rotation_speed.value.float_value = per_send;                           
-  homekit_characteristic_notify(&rotation_speed, rotation_speed.value);  // sending value to ios app
+//Called when the rotatio speed value is changed by iOS Home APP       iOS Home APP改变转速值(转速)时调用
+
+void rotation_speed_setter(const homekit_value_t v) 
+{ 
+    float per = v.float_value;
+    per = map(per, 0, 100, 0, 3);                                       // converting values from %(0-100) to pwm(0-1024) 
+    analogWrite(relay0, per);                                               // Setting the fan speed 
+    Serial.println(per);
+    set_pwm = per;                                                         // store pwm values to other variable
+    per_send = map(set_pwm, 0, 3, 0, 100);                              // converting values from pwm(0-1024) to %(0-100)
+    rotation_speed.value.float_value = per_send;                           
+    homekit_characteristic_notify(&rotation_speed, rotation_speed.value);  // sending value to ios app
 }
 
 void my_homekit_setup() {
 
-  //Add the .setter function to get the switch-event sent from iOS Home APP.
-  //The .setter should be added before arduino_homekit_setup.
-  //HomeKit sever uses the .setter_ex internally, see homekit_accessories_init function.
-  //Maybe this is a legacy design issue in the original esp-homekit library,
-  //and I have no reason to modify this "feature".
+    pinMode(relay0, OUTPUT);
+    pinMode(relay1, OUTPUT);
+    pinMode(relay2, OUTPUT);
+    pinMode(relay3, OUTPUT);
+    pinMode(button0, INPUT_PULLUP);
+    pinMode(button1, INPUT_PULLUP);
+    pinMode(button2, INPUT_PULLUP);
+    pinMode(button3, INPUT_PULLUP);
+    pinMode(PIN_SWITCH0, OUTPUT);
+	digitalWrite(PIN_SWITCH0, HIGH);
 
-  fan_on.setter = fan_on_setter;
-  
-  rotation_speed.setter = rotation_speed_setter;
-  
-  arduino_homekit_setup(&config);
+  //Add the .setter function to get the switch-event sent from iOS Home APP.                    添加.setter函数，获取iOS Home APP发送的switch-event。
+  //The .setter should be added before arduino_homekit_setup.                                   .setter 应该在 arduino_homekit_setup 之前添加
+  //HomeKit sever uses the .setter_ex internally, see homekit_accessories_init function.        HomeKit 服务器在内部使用 .setter_ex，请参阅 homekit_accessories_init 函数。
+  //Maybe this is a legacy design issue in the original esp-homekit library,                    也许这是原始 esp-homekit 库中的遗留设计问题，
+  //and I have no reason to modify this "feature".                                              我没有理由修改这个“功能”。
+    fan_on.setter = fan_on_setter;
+    cha_switch_on0.setter = cha_switch_on_setter0;
+    rotation_speed.setter = rotation_speed_setter;
+    arduino_homekit_setup(&config);
 
 
 
-  //  This function helps to change the value of the fan from on to off and vice-versa.    
-  ESPButton.add(0, buttonpin, LOW, true, true);
-  ESPButton.setCallback([&](uint8_t id, ESPButtonEvent event) {
+  //  This function helps to change the value of the fan from on to off and vice-versa.    此功能有助于将风扇的值从打开更改为关闭，反之亦然
+  // uint8_t _id, uint8_t _pin, uint8_t _pin_down_digital, bool _doubleclick_enable = false, bool _longclick_enable = true
+    ESPButton.add(0, button0, LOW, true, true);
+    // ESPButton.add(0, button1, LOW, true, true);
+    // ESPButton.add(0, button2, LOW, true, true);
+    // ESPButton.add(0, button3, LOW, true, true);
+    ESPButton.setCallback([&](uint8_t id, ESPButtonEvent event) {
 
-    if (event == ESPBUTTONEVENT_SINGLECLICK) {
-      if (analogRead(RELAY) == 0)
-      {
-        bool fan_con = true;
-        fan_on.value.bool_value = fan_con;
+    if (event == ESPBUTTONEVENT_SINGLECLICK) //单击按键时
+    {
+        if (digitalRead(relay0) == 0)
+        {
+            bool fan_con = true;                //闭合继电器
+            fan_on.value.bool_value = fan_con;
 
-        per_send = map(set_pwm, 0, 1024, 0, 100);
-        rotation_speed.value.float_value = per_send;
-        homekit_characteristic_notify(&fan_on, fan_on.value);
-        homekit_characteristic_notify(&rotation_speed, rotation_speed.value);
-        analogWrite(RELAY, set_pwm);
-        Serial.println("FAN IS ON NOW !!!!!");
-      }
-      else
-      {
-        bool fan_con = false;
-        fan_on.value.bool_value = fan_con;
-        rotation_speed.value.float_value = 0;
-        homekit_characteristic_notify(&fan_on, fan_on.value);
-        homekit_characteristic_notify(&rotation_speed, rotation_speed.value);
-        analogWrite(RELAY, 0);
-        Serial.println("FAN IS OFF NOW !!!!!");
-      }
+            per_send = map(set_pwm, 0, 3, 0, 100);
+            rotation_speed.value.float_value = per_send;
+            homekit_characteristic_notify(&fan_on, fan_on.value);
+            homekit_characteristic_notify(&rotation_speed, rotation_speed.value);
+            // analogWrite(relay0, set_pwm);
+            digitalWrite(relay0,HIGH);
+            Serial.println("FAN IS ON NOW !!!!!");
+        }
+        else
+        {
+            bool fan_con = false;               //松开继电器
+            fan_on.value.bool_value = fan_con;
+            rotation_speed.value.float_value = 0;
+            homekit_characteristic_notify(&fan_on, fan_on.value);
+            homekit_characteristic_notify(&rotation_speed, rotation_speed.value);
+            digitalWrite(relay0, LOW);
+            Serial.println("FAN IS OFF NOW !!!!!");
+        }
     }
 
   });
@@ -155,15 +185,17 @@ void my_homekit_setup() {
 
 
 
-void my_homekit_loop() {
-  arduino_homekit_loop();
-  ESPButton.loop();
-  const uint32_t t = millis();
-  if (t > next_heap_millis) {
-    // show heap info every 5 seconds
-    next_heap_millis = t + 5 * 1000;
-    LOG_D("Free heap: %d, HomeKit clients: %d",
-          ESP.getFreeHeap(), arduino_homekit_connected_clients_count());
+void my_homekit_loop() 
+{
+    arduino_homekit_loop();
+    ESPButton.loop();
+    const uint32_t t = millis();
+    if (t > next_heap_millis) 
+    {
+        // show heap info every 5 seconds               每 5 秒显示一次堆信息
+        next_heap_millis = t + 5 * 1000;
+        LOG_D("Free heap: %d, HomeKit clients: %d",
+            ESP.getFreeHeap(), arduino_homekit_connected_clients_count());
 
-  }
+    }
 }
--- 1_Farmware/1_Base_esp8266/FANtest/my_accessory.c
+++ 1_Farmware/1_Base_esp8266/FANtest/my_accessory.c
@@ -13,34 +13,43 @@ void my_accessory_identify(homekit_value_t _value) {
   printf("accessory identify\n");
 }
 
-// Switch (HAP section 8.38)
-// required: ON
+// Fan (HAP section 8.13)
+// required: Active
 // optional: ROTATION_SPEED
 //           ROTATION_DIRECTION
 //           NAME
 
-
+//一个电风扇总开关
 homekit_characteristic_t fan_on = HOMEKIT_CHARACTERISTIC_(ON, false);
+//一个控制LED
+homekit_characteristic_t cha_switch_on0 = HOMEKIT_CHARACTERISTIC_(ON, false);
 
+//三档调节风扇速度
 homekit_characteristic_t rotation_speed = HOMEKIT_CHARACTERISTIC_(ROTATION_SPEED, per_send);
-
+// format: string; HAP section 9.62; max length 64
+homekit_characteristic_t cha_name0 = HOMEKIT_CHARACTERISTIC_(NAME, "Switch0");
 
 homekit_accessory_t *accessories[] = {
-  HOMEKIT_ACCESSORY(.id = 1, .category = homekit_accessory_category_fan, .services = (homekit_service_t*[]) {
+  HOMEKIT_ACCESSORY(.id = 1, .category = homekit_accessory_category_fan, .services = (homekit_service_t*[]) {           //.id后边的数字代表带几个设备，>1就是作为桥连接
     HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics = (homekit_characteristic_t*[]) {
-      HOMEKIT_CHARACTERISTIC(NAME, "FAN"),
-      HOMEKIT_CHARACTERISTIC(MANUFACTURER, "HOMEKIT"),
-      HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "1234567"),
-      HOMEKIT_CHARACTERISTIC(MODEL, "FAN"),
-      HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "0.1"),
+      HOMEKIT_CHARACTERISTIC(NAME, "FANtest"),
+      HOMEKIT_CHARACTERISTIC(MANUFACTURER, "lycraft_Person"),
+      HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "666666"),
+      HOMEKIT_CHARACTERISTIC(MODEL, "ESP8266"),
+      HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "1.0"),
       HOMEKIT_CHARACTERISTIC(IDENTIFY, my_accessory_identify),
       NULL
     }),
-    HOMEKIT_SERVICE(FAN, .primary = true, .characteristics = (homekit_characteristic_t*[]) {
+    HOMEKIT_SERVICE(FAN, .primary = true, .characteristics = (homekit_characteristic_t*[]) {                             //建一个FAN类型服务
       &fan_on,
       &rotation_speed,
       NULL
     }),
+    HOMEKIT_SERVICE(SWITCH, .characteristics=(homekit_characteristic_t*[]){                          //新建一个服务 switch类型
+			&cha_switch_on0,
+			&cha_name0,
+			NULL
+	    }),
     NULL
   }),
   NULL
